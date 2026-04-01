a794a748607ac545237702d2db88741dfa654f54
r17e8h <riteshkr.rana18@gmail.com>
2026-04-01 15:14:39 +0530

    fix: update nextjs config for deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  reactStrictMode: false,
+  output: 'export',
+  basePath: '/CircuitSetu', 
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
